f2d5022cccf7cc56eeb35900c6775ef7786c6627
PedroViniciusM <115584060+PedroViniciusM@users.noreply.github.com>
2022-10-12 12:06:13 -0300

    jogos no pyhton em desenvolvimento
    o Jogo de forca ainda está em análise para desenvolvimento.

---
+++ Jogos/forca.py
@@ -0,0 +1,12 @@
+
+
+
+def jogar():
+    print('''
+    -----JOGO DE FORCA-----
+    ESCOLHA QUALQUER NÚMERO INTEIRO ATÉ 100 PARA JOGAR!
+    -----BOA SORTE!-----
+    ''')
+
+if __name__ == "__main__":
+    jogar()
